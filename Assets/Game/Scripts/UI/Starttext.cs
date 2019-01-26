486d4ed700f481d4bbec18767c1d224b50b6dc2e
qiaor1 <qiaor1@uci.edu>
2019-01-26 13:59:04 -0800

    resolve
    Signed-off-by: qiaor1 <qiaor1@uci.edu>

--- Assets/Game/Scripts/UI/Starttext.cs
+++ Assets/Game/Scripts/UI/Starttext.cs
@@ -1,18 +1,16 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.SceneManagement;
 
 public class Starttext : MonoBehaviour {
 
-    // Use this for initialization
-    void Update()
-    {
-        if (Input.anyKeyDown)
-        {
-            Application.LoadLevel("Tutorial");
-        }
-
-
-    }
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
 }
