6c7cec507e39357d22ec2aea4aeea272b9ef193f
demingz1 <demingz1@uci.edu>
2019-01-26 13:40:03 -0800

    3d object and circuit prototype
    Signed-off-by: demingz1 <demingz1@uci.edu>

---
+++ Assets/Game/Scripts/Circuit.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Circuit : MonoBehaviour {
+    public List<Vector3> nodes = new List<Vector3>();
+    public int cnt = 0;
+	// Use this for initialization
+	void Start () {
+        
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+    void OnDrawGizmos()
+    {
+
+        while (cnt < nodes.Count - 1)
+        {
+            print("sss");
+
+            Gizmos.color = Color.red;
+            Gizmos.DrawLine(nodes[cnt], nodes[cnt + 1]);
+            cnt++;
+
+        }
+        cnt = 0;
+    }
+}
