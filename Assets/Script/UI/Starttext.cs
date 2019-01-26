a298bab974ee901bed935af8cbcbf28c472cbbe2
qiaor1 <qiaor1@uci.edu>
2019-01-26 12:54:32 -0800

    to

--- Assets/Script/UI/Starttext.cs
+++ Assets/Script/UI/Starttext.cs
@@ -1,16 +1,15 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class Starttext : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+    // Use this for initialization
+    public void StartGame()
+    {
+            SceneManager.LoadScene("Tutorial");
+ 
+        
+    }
 }
