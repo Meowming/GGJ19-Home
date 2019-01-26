1a12b93a920db7696cb3d696e16d6f8b77b96b84
qiaor1 <qiaor1@uci.edu>
2019-01-26 14:10:18 -0800

    after the change

--- Assets/Game/Scripts/UI/Starttext.cs
+++ Assets/Game/Scripts/UI/Starttext.cs
@@ -4,13 +4,12 @@ using UnityEngine;
 
 public class Starttext : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
+
 	// Update is called once per frame
 	void Update () {
-		
+		if(Input.anyKey)
+        {
+            Application.LoadLevel("Tutorial");
+        }
 	}
 }
