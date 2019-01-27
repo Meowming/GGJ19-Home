45c9f44b25cf546acd97bed0ae9f44b32bd32d52
qiaor1 <qiaor1@uci.edu>
2019-01-27 10:39:44 -0800

    tutoral

---
+++ Assets/Game/Scripts/Callingtrans.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Callingtrans : MonoBehaviour {
+
+    // Use this for initialization
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Player"))
+        {
+            SceneManager.LoadScene("calling cg");
+        }
+
+    }
+    
+}
