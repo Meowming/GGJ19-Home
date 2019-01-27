2ee388bf3df26edc88639ed6bb5c63a66894cb1b
qiaor1 <qiaor1@uci.edu>
2019-01-26 20:57:28 -0800

    dia

---
+++ Assets/Game/Scripts/NPCBehavior.cs
@@ -0,0 +1,63 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class NPCBehavior : MonoBehaviour
+{
+    public TextMeshProUGUI textDisplay;
+    public string[] lines;
+    private int index = 0;
+    private float typingSpeed = 0.005f;
+    public GameObject diaBox;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    IEnumerator Type()
+    {
+        foreach (char letter in lines[index].ToCharArray())
+        {
+            textDisplay.text += letter;
+            yield return new WaitForSeconds(typingSpeed);
+        }
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Player"))
+        {
+            StartCoroutine(Type());
+            diaBox.SetActive(true);
+        }
+        else
+        {
+            diaBox.SetActive(false);
+        }
+
+    }
+    void NextLine()
+    {
+        if (index < lines.Length - 1)
+        {
+            index++;
+            textDisplay.text = "";
+            StartCoroutine(Type());
+        }
+        else
+        {
+            textDisplay.text = "";
+        }
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        if (textDisplay.text == lines[index] && Input.GetKeyDown(KeyCode.Space))
+        {
+            NextLine();
+        }
+    }
+}
