c7f82e0e1840b6ce81027167f4b8cc636a5be3fc
Spitwater <Spitwater@users.noreply.github.com>
2019-01-26 21:54:36 -0800

    Level4

---
+++ Assets/Game/Scripts/Laser.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Laser : MonoBehaviour {
+    [SerializeField] private float xDirection;
+    [SerializeField] private float yDirection;
+    [SerializeField] private float distance;
+    private Vector2 direction;
+
+	// Use this for initialization
+	void Start () {
+        direction = new Vector2(xDirection, yDirection);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        RaycastHit2D laserBean = Physics2D.Raycast(transform.position, direction, distance);
+
+        if(laserBean.collider.CompareTag("player"))
+        {
+
+        }
+	}
+}
