47ba2f1c05fa87750922f8bad3e76be80f231798
qiaor1 <qiaor1@uci.edu>
2019-01-26 16:20:45 -0800

    endgame

--- Assets/Game/Scripts/UI/endgame.cs
+++ Assets/Game/Scripts/UI/endgame.cs
@@ -1,16 +1,15 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEditor;
 
 public class endgame : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+    // Use this for initialization
+    public void Quit()
+    {
+        Application.Quit();
+        
+
+    }
 }
