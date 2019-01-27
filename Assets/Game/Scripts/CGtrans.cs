8419c2d9313c3bb584936861bb0781fc2a9ad89d
qiaor1 <qiaor1@uci.edu>
2019-01-27 11:26:35 -0800

    cg

---
+++ Assets/Game/Scripts/CGtrans.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CGtrans : MonoBehaviour
+{
+    public GameObject pausePlayer;
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            pausePlayer.SetActive(true);
+        }
+    }
+}
