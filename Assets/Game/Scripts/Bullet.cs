067588856090932dbb023ad52bc0883ff25034ea
Spitwater <Spitwater@users.noreply.github.com>
2019-01-26 21:05:05 -0800

    Level4

---
+++ Assets/Game/Scripts/Bullet.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bullet : MonoBehaviour {
+    private GameObject target;
+    private Rigidbody2D rgbd2d;
+    [SerializeField] private bool guided;
+    [SerializeField] private float speed;
+
+	// Use this for initialization
+	void Start () {
+        rgbd2d = GetComponent<Rigidbody2D>();
+        Vector2 direction = (target.transform.position - transform.position).normalized;
+
+        rgbd2d.velocity = direction * speed;
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		if(guided)
+        {
+            Vector2 direction = (target.transform.position - transform.position).normalized;
+
+            rgbd2d.velocity = direction * speed;
+        }
+	}
+
+    public void SetTarget(GameObject Target)
+    {
+        target = Target;
+    }
+}
---
+++ Assets/Game/Scripts/EnemyAttack.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyAttack : MonoBehaviour {
+
+    [SerializeField] private GameObject target;
+    [SerializeField] private float attackDistance;
+    [SerializeField] private float attackInterval;
+    [SerializeField] private GameObject bullet;
+    private Vector2 firePosition;
+    private float nextAttack;
+
+	// Use this for initialization
+	void Start () {
+        nextAttack = Time.time + attackInterval;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (target == null)
+            return;
+
+        if((target.transform.position.x - transform.position.x) < 0)
+        {
+            transform.localScale = new Vector3(-1, 1, 1);
+        }
+        else
+        {
+            transform.localScale = new Vector3(1, 1, 1);
+        }
+
+
+        if (Time.time >= nextAttack)
+        {
+            if((target.transform.position - transform.position).magnitude <= attackDistance)
+                Attack();
+
+        }
+	}
+
+    void Attack()
+    {
+        if(transform.localScale.x > 0)
+        {
+            firePosition = transform.position + Vector3.right * 0.5f;
+        }
+        else
+        {
+            firePosition = transform.position + Vector3.left * 0.5f;
+        }
+
+        GameObject bulletInstance = Instantiate(bullet, firePosition, transform.rotation);
+        Bullet bulletScript = bulletInstance.GetComponent<Bullet>();
+        bulletScript.SetTarget(target);
+
+        nextAttack = Time.time + attackInterval;
+    }
+}
